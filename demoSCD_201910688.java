3893521d0e0f441213fe75a5ec519d4ab0ae3221
AlaaAlbayed <alaa.albayed2016@gmail.com>
2021-10-26 15:58:32 +0300

    Added a default value

--- demoSCD_201910688.java
+++ demoSCD_201910688.java
@@ -1,7 +1,12 @@
 public class demoSCD_201910688
 {
 	public static void main(String args[]){
-		System.out.println("Hello, Word");
+		String name = "word";
+		if (args.length != 0)
+		{
+			name = args[0];
+		}
+			System.out.println("Hello, " + name + "!");
 	}
 }
 
